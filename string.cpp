f80a5338e849510e0428095f3ba23fe3d89119ad
derzouli <schong@e1r8p20.42.fr>
2016-08-04 05:44:44 +0200

    string is a compound type of the string class

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string mystring;
+
+  mystring = "the first string, im going to print";
+  cout << mystring << endl;
+  mystring = "toto";
+  cout << mystring << endl;
+  return 0;
+}
